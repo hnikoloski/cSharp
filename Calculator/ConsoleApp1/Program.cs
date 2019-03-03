3f20908a50e3131111ce186d4829445d5101b44a
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-03-03 21:36:51 +0100

    Calculator HomeWork Push

---
+++ Calculator/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Users First Value
+            Console.WriteLine("Enter your first number: ");
+            double firstNumber;
+            if (!double.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.WriteLine("Enter a valid input for the first number");
+            }
+                    
+            //Operation
+            Console.WriteLine("Enter opperation (+,-,*,/)");
+            string stringOperation = Console.ReadLine();
+
+            //Users second value
+            Console.WriteLine("Enter your second number: ");
+            double secondNumber;
+            if (!double.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.WriteLine("Enter a valid input for the second number.");
+            }
+
+            //Operation logic
+            int operation = 0;
+            if (stringOperation == "+")
+            {
+                operation = 1;
+            }
+            else if (stringOperation == "-")
+            {
+                operation = 2;
+            }
+            else if (stringOperation == "*")
+            {
+                operation = 3;
+
+            }
+            else if (stringOperation == "/")
+            {
+                operation = 4;
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid operation");
+            }
+            //Calculations 
+            double result = 0;
+            switch (operation)
+            {
+                case 1:
+                    result = firstNumber + secondNumber;
+                    break;
+                case 2:
+                    result = firstNumber - secondNumber;
+                    break;
+                case 3:
+                    result = firstNumber * secondNumber;
+                    break;
+                case 4:
+                    result = firstNumber / secondNumber;
+                    break;
+            }
+            Console.WriteLine("Result of " + firstNumber + " " + stringOperation + " " + secondNumber + " = " + result);
+            Console.ReadLine();
+        }
+    }
+}
