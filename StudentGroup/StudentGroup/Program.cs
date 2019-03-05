54812cb3c82bf465c6a50b89f096580e88d660cf
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-03-05 23:40:17 +0100

    First Commint to studentGroup I need to add Infinite loop

---
+++ StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] studentsG1 = new string[5] { "Zdravko", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
+            string[] studentsG2 = new string[5] { "Zdravko2", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
+            string[] studentsG3 = new string[5] { "Zdravko3", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
+
+            Console.WriteLine("enter student group: (There are 1,2 and 3 ) ");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine(string.Join(",", studentsG1)); 
+                    break;
+                case "2":
+                    Console.WriteLine(string.Join(",", studentsG2));
+
+                    break;
+                case "3":
+                    Console.WriteLine(string.Join(",", studentsG3));
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again");
+                    break;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
