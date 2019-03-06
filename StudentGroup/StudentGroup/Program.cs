de917eaea26fc21f8decb496d0919cfd96268264
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-03-06 11:58:18 +0100

    Exit Option added

--- StudentGroup/StudentGroup/Program.cs
+++ StudentGroup/StudentGroup/Program.cs
@@ -11,35 +11,45 @@ namespace StudentGroup
     {
         static void Main(string[] args)
         {
-
-            string[] studentsG1 = new string[5] { "Zdravko", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
-            string[] studentsG2 = new string[5] { "Zdravko2", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
-            string[] studentsG3 = new string[5] { "Zdravko3", "Zdravko", "Zdravko", "Zdravko", "Zdravko" };
-
-            Console.WriteLine("enter student group: (There are 1,2 and 3 ) ");
-            string userInput = Console.ReadLine();
-
-            switch (userInput)
+            while (true)
             {
-                case "1":
-                    Console.WriteLine(string.Join(",", studentsG1)); 
-                    break;
-                case "2":
+
+
+                string[] studentsG1 = new string[5] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+                string[] studentsG2 = new string[5] { "Anastasija", "Andrej", "Damjan", "David", "Jovan" };
+                string[] studentsG3 = new string[5] { "Marko", "Petar", "Stefan", "Angela", "Dimitar" };
+
+
+                Console.WriteLine("enter student group: (There are 1,2 and 3 if you want to exit enter E) ");
+                string userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "1")
+                {
+                    Console.WriteLine(string.Join(",", studentsG1));
+                    Console.WriteLine("-----------------------------------------------------------------");
+
+                }
+                else if (userInput == "2")
+                {
                     Console.WriteLine(string.Join(",", studentsG2));
-
-                    break;
-                case "3":
+                    Console.WriteLine("-----------------------------------------------------------------");
+                }
+                else if (userInput == "3")
+                {
                     Console.WriteLine(string.Join(",", studentsG3));
+                    Console.WriteLine("-----------------------------------------------------------------");
+                }
 
+                else if (userInput == "E")
+                {
                     break;
-                default:
-                    Console.WriteLine("Invalid input. Please try again");
-                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a valid input");
+                }
             }
-
-
-
-            Console.ReadLine();
-        }
+ 
+        } 
     }
 }
