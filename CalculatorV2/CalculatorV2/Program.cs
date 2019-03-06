5d934abc7b083461ac21ca44dbf9b5486d022c91
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-03-06 15:27:53 +0100

    Calculator V2 Upload

---
+++ CalculatorV2/CalculatorV2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+
+
+                //Users First Value
+               
+                double firstNumber;
+                double secondNumber;
+                string stringOperation;
+
+
+                do
+                {
+                    Console.WriteLine("Enter Value for the first number, Must be a number!!");
+
+                }
+                while (!double.TryParse(Console.ReadLine(), out firstNumber));
+                do
+                {
+                    Console.WriteLine("Enter opperation (+,-,*,/)");
+                    stringOperation = Console.ReadLine();
+
+
+                } 
+                // I have no idea why it works with && and not with ||
+                while (stringOperation != "+" && stringOperation != "-" && stringOperation != "*" && stringOperation != "/"  );
+
+                do
+                {
+                    Console.WriteLine("Enter Value for the second number, Must be a number!!");
+
+                }
+                while (!double.TryParse(Console.ReadLine(), out secondNumber));
+
+
+                //Operation
+                
+                //Operation logic
+                //int operation = 0;
+                //if (stringOperation == "+")
+                //{
+                //    operation = 1;
+                //}
+                //else if (stringOperation == "-")
+                //{
+                //    operation = 2;
+                //}
+                //else if (stringOperation == "*")
+                //{
+                //    operation = 3;
+                //}
+                //else if (stringOperation == "/")
+                //{
+                //    operation = 4;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Enter a valid operation");
+                //}
+                //Calculations 
+                double result = 0;
+                switch (stringOperation)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        break;
+                }
+                Console.WriteLine("Result of " + firstNumber + " " + stringOperation + " " + secondNumber + " = " + result);
+                
+                Console.WriteLine("Do you want to exit? y/n? ");
+                string exit = Console.ReadLine();
+                if (exit == "n")
+                {
+                    continue;
+                }
+                else if (exit == "y")
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
