01e7b4249291e37bc63442e698a1daeb82192562
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-03-07 23:50:33 +0100

    HomeWork AgeCalculator First Commit

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static DateTime AgeCalc()
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                Console.WriteLine("Enter your birthdate in the following format: MM/DD/YYYY  ");
+                DateTime birthDate;
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    TimeSpan age = now - birthDate;
+                    Console.WriteLine($"Your age is {age.Days / 365} years and {age.Days % 365} days");
+
+
+                    Console.WriteLine("Do you want to exit? Y/N ?  \n\n\n");
+                    string exit = Console.ReadLine().ToUpper();
+              
+
+                }
+                else Console.WriteLine("invalid date.");
+
+
+                
+              
+                            
+                
+            }
+        }
+      
+        static void Main(string[] args)
+        {
+            AgeCalc();
+        }
+    }
+}
